c05ebfd60e022a498dbe321993d0881b21ae7ab5
Leo Liao <honglin0822@outlook.com>
2022-01-28 20:44:44 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from '@app/Link';
 
-const App = () => <Link page="https://www.google.com/">Hello World</Link>;
+const App = () => <Link page="https://www.google.com/">Hello World!</Link>;
 
 export default App;
