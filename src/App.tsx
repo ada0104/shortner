40cccfaeba1edf835743ee857d0871909fb54240
Leo Liao <honglin0822@outlook.com>
2022-03-13 16:29:09 +0800

    CU-2dum5w3 - React Environment

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import { Routes, Route } from 'react-router-dom';
-import Provider from './component/util/provider.component';
+import Root from './component/util/root.component';
 import Expenses from './container/expenses.container';
 import Invoices from './container/invoices.container';
+import { environment } from './core/environment';
 
-const App = () => (
-  <Provider>
-    <Routes>
-      <Route path="/expenses" element={<Expenses />} />
-      <Route path="/invoices" element={<Invoices />} />
-      <Route path="/" element={<h1>Home</h1>} />
-      <Route
-        path="*"
-        element={
-          <main style={{ padding: "'1rem'" }}>
-            <p>There is nothing here!</p>
-          </main>
-        }
-      />
-    </Routes>
-  </Provider>
-);
+const App = () => {
+  return (
+    <Root>
+      <Routes>
+        <Route path="/expenses" element={<Expenses />} />
+        <Route path="/invoices" element={<Invoices />} />
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: "'1rem'" }}>
+              <p>There is nothing here!</p>
+            </main>
+          }
+        />
+      </Routes>
+    </Root>
+  );
+};
 
 export default App;
--- src/container/expenses.container.tsx
+++ src/container/expenses.container.tsx
@@ -1,9 +1,12 @@
+import BasePage from '@app/component/util/base-page.component';
 import React from 'react';
 
 const Expenses = () => (
-  <main style={{ padding: '1rem 0' }}>
-    <h2>Expenses</h2>
-  </main>
+  <BasePage>
+    <main style={{ padding: '1rem 0' }}>
+      <h2>Expenses</h2>
+    </main>
+  </BasePage>
 );
 
 export default Expenses;
