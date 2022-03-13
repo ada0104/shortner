4115f700cdccbf2e14b55174e2307bad58ff37a1
Yuni Chou <yuni.chou@cloud-interactive.com>
2022-03-13 20:50:34 +0800

    Fix merge conflict

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Root from './component/util/root.component';
 import Expenses from './container/expenses.container';
 import Invoices from './container/invoices.container';
+import MUI from './component/test/mui.component';
 import { environment } from './core/environment';
 
 const App = () => {
@@ -10,7 +11,7 @@ const App = () => {
       <Routes>
         <Route path="/expenses" element={<Expenses />} />
         <Route path="/invoices" element={<Invoices />} />
-        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/" element={<MUI />} />
         <Route
           path="*"
           element={
---
+++ src/component/test/mui.component.tsx
@@ -0,0 +1,12 @@
+import Button from '@mui/material/Button';
+
+const MUI = () => {
+  return (
+    <>
+      <Button variant="contained">你好，世界</Button>
+      <div className="text-blue-500">tailwind</div>
+    </>
+  );
+};
+
+export default MUI;
