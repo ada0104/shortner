dd53cb2de8d6dde99ab282e9321f9a4e22e745f6
Leo Liao <honglin0822@outlook.com>
2022-03-15 20:14:16 +0800

    CU-2fe1eqg - Feature Route Sample

--- src/component/util/header.component.tsx
+++ src/component/util/header.component.tsx
@@ -1,16 +1,22 @@
-import { Link } from 'react-router-dom';
+import { Link } from '@mui/material';
 
 const Header = () => {
   const style = {
     borderBottom: 'solid 1px',
-    paddingBottom: '1rem',
   };
 
   return (
     <div>
       <nav style={style}>
-        <Link to="/invoices">Invoices</Link>
-        <Link to="/expenses">Expenses</Link>
+        <Link className="m-8" underline="hover" href="/test/index">
+          Test Index
+        </Link>
+        <Link className="m-8" underline="hover" href="/test/a">
+          Test A
+        </Link>
+        <Link className="m-8" underline="hover" href="/test/b">
+          Test B
+        </Link>
       </nav>
     </div>
   );
---
+++ src/feature/test/test-index/test-index.container.tsx
@@ -0,0 +1,11 @@
+import BasePage from '@app/component/util/base-page.component';
+
+const TestIndex = () => {
+  return (
+    <BasePage>
+      <h2>Test Index</h2>
+    </BasePage>
+  );
+};
+
+export default TestIndex;
