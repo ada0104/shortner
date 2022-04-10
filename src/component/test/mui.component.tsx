2f51368a0d3120b4ae285e8a87c67c57512d792a
Leo Liao <honglin0822@outlook.com>
2022-04-10 15:37:34 +0800

    CU-2k0gy4u - Google OAuth Login

--- src/component/test/mui.component.tsx
+++ src/component/test/mui.component.tsx
@@ -1,5 +1,6 @@
 import Button from '@mui/material/Button';
 import { useNavigate } from 'react-router-dom';
+import GoogleLoginBtn from '../auth/google-login.component';
 
 const MUI = () => {
   const nav = useNavigate();
@@ -13,6 +14,7 @@ const MUI = () => {
       <Button variant="contained" onClick={navigate}>
         你好，世界
       </Button>
+      <GoogleLoginBtn />
       <div className="text-blue-500">tailwind</div>
     </>
   );
