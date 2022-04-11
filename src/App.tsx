38592d77dbce51ed9a3d2259c996cc94dbdd8237
Leo Liao <honglin0822@outlook.com>
2022-04-11 23:16:16 +0800

    CU-2k0j50t - axios instance

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { serviceOptions } from 'api/index.defs';
+import axios from 'axios';
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
 import AppRoute from './app.route';
 import Root from './component/util/root.component';
+import { environment } from './core/environment';
 import { store } from './store/store';
+
+// create axios instance
+serviceOptions.axios = axios.create({
+  baseURL: environment.api_host,
+  timeout: 1000 * 60,
+});
 
 const App = () => {
   return (
