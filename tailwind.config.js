6fc6f78fc9028b1e1de292a69df5be0938f00c78
Yuni Chou <yuni.chou@cloud-interactive.com>
2022-04-18 22:00:44 +0800

    Tailwind basic config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@ module.exports = {
   content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
   important: '#root',
   theme: {
-    extend: {},
+    fontFamily: {
+      'main-noto': ['Noto Sans TC', 'sans-serif'],
+      'secondary-roboto': ['Roboto', 'sans-serif'],
+    },
+    extend: {
+      color: {
+        'main-blue': {
+          100: '#F9FBFF',
+          200: '#E7EEFA',
+          300: '#3064B9',
+        },
+        'secondary-gary': {
+          100: '#FAFAFA',
+          200: '#E5E5E5',
+          300: '#C4C4C4',
+          400: '#A5A5A5',
+          500: '#868686',
+        },
+        'secondary-red': '#D95140',
+        'secondary-blue': '#1877F2',
+        'base-back-gray': '#FAFAFA',
+      },
+    },
   },
   plugins: [],
 };
